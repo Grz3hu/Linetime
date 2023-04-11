275a3ef4cb74c12de5246388f78fe111f720d28b
Grz3hu <grek222@o2.pl>
2023-04-11 23:47:54 +0200

    added endpoint for adding, deleting and viewing user

--- backend/src/main/java/com/linetime/backend/controller/UserController.java
+++ backend/src/main/java/com/linetime/backend/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.linetime.backend.controller;
 
+import com.linetime.backend.exception.UserNotFoundException;
 import com.linetime.backend.model.User;
 import com.linetime.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3001")
 public class UserController {
 
     @Autowired
@@ -17,4 +19,36 @@ public class UserController {
     User addUser(@RequestBody User newUser){
         return userRepository.save(newUser);
     }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Integer id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Integer id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(newUser.getName());
+                    user.setSurname(newUser.getSurname());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Integer id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return  "User with id "+id+" has been deleted successfully.";
+    }
+
 }
