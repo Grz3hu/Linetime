2fd992c8bdd005e76cf09b28da5fe24427d70c5a
Grz3hu <grek222@o2.pl>
2023-04-12 17:37:54 +0200

    Added logo

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import logo from "../images/logo.png"
 
 export default function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Linetime</a>
+                    <a className="navbar-brand" href="/"><img src={logo} width='140vw' /></a>
 
                     <div>
                         <Link className='btn btn-outline-light me-2' to="/login">Login</Link>
