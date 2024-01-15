0f195f15aa68d8ca69a910ef33279b2483c58de8
Grzegorz Świstak <grzegorz@example.com>
2024-01-15 20:17:20 +0100

    added script that applies k8s resources

---
+++ k8s/apply.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+kubectl apply -f linetime-namespace.yaml
+
+kubectl apply -f backend/backend-deployment.yaml
+kubectl apply -f backend/backend-service.yaml
+kubectl apply -f backend/backend-ingress.yaml
+kubectl apply -f backend/backend-autoscaler.yaml
+
+kubectl apply -f db/postgres-config.yaml
+kubectl apply -f db/postgres-secret.yaml
+kubectl apply -f db/postgres-init-script.yaml
+kubectl apply -f db/postgres-kubegres.yaml
+
+kubectl apply -f frontend/frontend-deployment.yaml
+kubectl apply -f frontend/frontend-service.yaml
+kubectl apply -f frontend/frontend-ingress.yaml
+kubectl apply -f frontend/frontend-autoscaler.yaml
+
+sleep 8
+kubectl exec -it -n linetime pods/postgres-1-0 -- sh -c $'export PGPASSWORD=postgresSuperUserPsw; psql -U postgres linetime_db -c "INSERT INTO roles (name) VALUES (\'ROLE_USER\');"'
+kubectl exec -it -n linetime pods/postgres-1-0 -- sh -c $'export PGPASSWORD=postgresSuperUserPsw; psql -U postgres linetime_db -c "INSERT INTO roles (name) VALUES (\'ROLE_ADMIN\');"'
